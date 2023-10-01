f77a9a4f01792741f0fa4179d05688638b79abe7
Matěj Kříž <lyzynec@protonmail.com>
2023-10-02 01:23:49 +0200

    Fixed windows issue.

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,28 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use signal_hook;
 
 pub struct EndSignal {
-  pub end: Arc<AtomicBool>,
+  pub end_manual: Arc<AtomicBool>,
+  pub end_systemd: Arc<AtomicBool>,
 }
 
 impl EndSignal {
   pub fn new() -> Result<EndSignal, Error> {
 
-    let out = EndSignal { end: Arc::new(AtomicBool::new(false)), };
+    let out = EndSignal {
+      end_manual: Arc::new(AtomicBool::new(false)),
+      end_systemd: Arc::new(AtomicBool::new(false)),
+    };
 
     // manual end signal
-    signal_hook::flag::register(signal_hook::consts::SIGINT, std::sync::Arc::clone(&out.end))?;
+    signal_hook::flag::register(signal_hook::consts::SIGINT, std::sync::Arc::clone(&out.end_manual))?;
 
     // systemd end signal
-    signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&out.end))?;
+    signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&out.end_systemd))?;
 
     return Ok(out);
   }
 
   pub fn is_end(&self) -> bool {
-    return self.end.load(Ordering::Relaxed);
+    return self.end_manual.load(Ordering::Relaxed) || self.end_systemd.load(Ordering::Relaxed);
   }
 }
